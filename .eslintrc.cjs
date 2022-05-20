30a5770dde59340b96dec8f8a7d583860f1e6c8a
G100 <g100my.lan229@gmail.com>
2022-05-21 03:25:52 +0800

    chore: install eslint-config-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
   env: {
     'vue/setup-compiler-macros': true,
   },
@@ -16,4 +16,4 @@ module.exports = {
   rules: {
     'vue/multi-word-component-names': 'off',
   },
-}
+};
