305210c3e95fa31efd35cd29814033fd62e1b6ed
G100 <g100my.lan229@gmail.com>
2022-05-22 02:05:57 +0800

    feat(chore): install unplugin-auto-import, init

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Pages from 'vite-plugin-pages';
 import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages(), Components()],
+  plugins: [vue(), Pages(), Components(), AutoImport()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
