735014c2581c108f3251eb1320a9ce1769c1fe0a
G100 <g100my.lan229@gmail.com>
2022-05-22 02:16:36 +0800

    feat(chore): auto-import config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,25 @@ import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages(), Components(), AutoImport()],
+  plugins: [
+    vue(),
+    Pages(),
+    Components({}),
+    AutoImport({
+      include: [
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+      ],
+      imports: ['vue', 'vue-router', 'pinia'],
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
